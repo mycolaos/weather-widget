ff850cef869fd9fc2ac8f528fb037c88b8f5e1ba
Mycolaos <mchornobay@altereos.com>
2018-05-11 22:06:47 +0200

    removed useless code; corrected classname

--- src/FlipCard/index.js
+++ src/FlipCard/index.js
@@ -2,15 +2,9 @@ import React, { Component } from 'react'
 import './styles.scss'
 
 class FlipCard extends Component { 
-  state = { active: false };
-
-  toggle = () => {
-    this.setState({ active: !this.state.active})
-  }
-
   render() {
     const { frontComponent, backComponent, showBack } = this.props
-    const className = showBack ? 'active' : ''
+    const className = showBack ? 'flipcard--active' : ''
     
     return (
       <div className={`flipcard ${className}`}>
